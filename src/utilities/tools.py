e346bf97cc45eead597a00157346076b66a371f3
Diego V <diego.v@bineo.com>
2021-09-09 18:44:57 -0500

    payaseo de estilos

--- src/utilities/tools.py
+++ src/utilities/tools.py
@@ -1,46 +1,41 @@
 import json
-from jsonschema import validate
-import os
 from datetime import date
-import re
+from jsonschema import validate
+from pathlib import Path
 
-FILE_PATH = os.path.dirname(__file__)
+ROOT = Path(__file__).parents[2]
 
-def validate_input(payl):
-    with open(
-        os.path.join(
-            FILE_PATH,
-            'input_schema.json'
-        ), 'r'
-    ) as j:
-        input_schema = json.load(j)
 
-    validate(instance=payl, schema=input_schema)
+def validate_input(payload):
+    with open(ROOT/'openapi'/'1-rfc-input.json', 'r') as _f:
+        input_schema = json.load(_f)
+    validate(instance=payload, schema=input_schema)
 
-def select_subdict(dictionary, keys_to_select):
-    for k in keys_to_select:
-        if k not in dictionary.keys():
-            raise ValueError('Trying to select a subdict with keys not contained on large dict.')
-    
-    dict_filter = lambda x, y: dict([ (i,x[i]) for i in x if i in set(y) ])
-    small_dict=dict_filter(dictionary, keys_to_select)
+
+def select_subdict(a_dict, sub_keys):
+    if not sub_keys.is_subset(a_dict.keys()):
+        raise ValueError('Trying to select a subdict with keys not contained on large dict.')
+    small_dict = dict((k, a_dict[k]) for k in sub_keys)
     return small_dict
+
 
 def camel(snake_str):
     first, *others = snake_str.split('_')
     return ''.join([first.lower(), *map(str.title, others)])
 
+
 def camelize_dict(snake_dict):
-    new_dict = {}
-    if isinstance(snake_dict, dict):
-        for key, value in snake_dict.items():
-            new_key = camel(key)
-            if isinstance(value, list):
-                new_dict[new_key] = list(map(camelize_dict, value))
-            if isinstance(value, dict):
-                new_dict[new_key] = camelize_dict(value)
-            else:
-                new_dict[new_key] = value
-        return new_dict
-    else:
+    if not isinstance(snake_dict, dict):
         return snake_dict
+
+    def pass_value(a_val): 
+        if isinstance(a_val, list):
+            passed = list(map(camelize_dict, a_val))
+        elif isinstance(a_val, dict):
+            passed = camelize_dict(a_val)
+        else:
+            passed = a_val 
+        return passed
+
+    new_dict = dict((k, pass_value(v)) for (k, v) in snake_dict.items())
+    return new_dict
---
+++ tests/test-rfc.py
@@ -0,0 +1,36 @@
+import requests
+from unittest import TestCase
+
+
+URL = "https://wap-validation-services-dev.azurewebsites.net/rfc"
+
+
+class RfcTestCase(TestCase):
+    def setup(self):
+        pass
+
+    def test_correct_usage(self):
+        myself = {
+            "input" : { "personPhysical" : { 
+                "firstName"         : "Diego", 
+                "lastName"          : "Villamil", 
+                "maternalLastName"  : "Pesqueira", 
+                "dateOfBirth"       : "1983-12-27"}}, 
+            "output"    : {
+                "rfc"   : "VIPD831227DH2"} }
+
+        response = requests.post(URL, json=myself["input"])
+        obtained = response.json()
+        self.assertEqual(obtained, myself["output"])
+
+    def test_missing_arguments(self):
+        w_missing = { "input" : { "personPhysical" : {
+            "lastName"          : "Villamil", 
+            "maternalLastName"  : "Pesqueira", 
+            "dateOfBirth"       : "1983-12-27"
+            } }, 
+            "output" : { "error" : "An Error" } 
+        }
+        
+        response = requests.post(URL, json=w_missing["input"])
+        self.assertEqual(response)
